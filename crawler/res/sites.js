3edd2afef024ab6a818c80286560384860d4565e
Roberto Fernandes <betocostaf@gmail.com>
2018-09-12 22:35:07 -0300

    Adding robots.txt to check links

---
+++ crawler/res/sites.js
@@ -0,0 +1,15 @@
+var sites = [
+    "https://www.kijiji.ca/",
+    "https://shift.com/",
+    "http://www.munstermanauto.com/",
+    "https://www.marblesautomotive.com/",
+    "https://www.kbb.com/",
+    "https://www.usaa.com/",
+    "https://www.usedcars.com/",
+    "https://www.carsforsale.com/",
+    "https://www.autotrader.com/",
+    "https://www.cars.com/",
+    "https://www.buyacar.co.uk/"
+];
+
+module.exports = sites;
