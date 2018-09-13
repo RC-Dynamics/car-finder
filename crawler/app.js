0217143752b5d7ebd86b238d9870de8fd0292412
Roberto Fernandes <betocostaf@gmail.com>
2018-09-12 22:35:07 -0300

    Adding robots.txt to check links

--- crawler/app.js
+++ crawler/app.js
@@ -1,7 +1,15 @@
 const request = require('request');
 
-request({
-    url: "https://www.kijiji.ca/"
-}, (err, res, body) => {
-    console.log(body);
+const sites = require('./res/sites.js');
+
+sites.forEach(site => {
+    request({
+        url: site + "robots.txt"
+    }, (err, res, body) => {
+        console.log(`${site} - ${res.statusCode}`);
+        if (res.statusCode == 200){
+            console.log(body);
+        }
+    });
+    
 });
