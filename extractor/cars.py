14d7ae2728e9625300498243b6b24a814f6fc460
jailson <jailson@motorola.com>
2018-09-17 11:29:15 -0300

    Ok table

--- extractor/cars.py
+++ extractor/cars.py
@@ -11,18 +11,22 @@ tabela = soup.find_all(class_="vdp-details-basics__item")
 for item in tabela:
     if(item.get_text().split()[0] == "Fuel"):
         fuel = item.get_text().split(":")[-1]
+        fuel = fuel.strip().replace('\n','')
     elif(item.get_text().split()[0] == "Exterior"):
         exterior_color = item.get_text().split(":")[-1]
+        exterior_color = exterior_color.strip().replace('\n','')
     elif(item.get_text().split()[0] == "Interior"):
         interior_color = item.get_text().split(":")[-1]
+        interior_color = interior_color.strip().replace('\n','')
     elif(item.get_text().split()[0] == "Engine:"):
         engine = item.get_text().split(":")[-1]
+        engine = engine.strip().replace('\n','')
     elif(item.get_text().split()[0] == "VIN:"):
         vin = item.get_text().split(":")[-1]
+        vin = vin.strip().replace('\n','')
 
 print(fuel)
 print(str(exterior_color))
 print(str(interior_color))
 print(str(engine))
 print(str(vin))
-
