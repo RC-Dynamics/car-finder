bef25e421af2907afcd262e97cd0460d438a80fe
Roberto Fernandes <betocostaf@gmail.com>
2018-09-17 17:46:16 -0300

    Starting develop bfs visit

--- crawler/bfs/bfs.py
+++ crawler/bfs/bfs.py
@@ -1,8 +1,41 @@
+import requests
+import time
+from bs4 import BeautifulSoup
 import sys
 sys.path.append('../shared')
 
 from pre_processing import PreProcessing
 
+class CrawlerBFS:
+    url = ''
+    order = []
+    disallow = []
+    MAX_VISITS = 1
+
+    def __init__(self, site):
+        self.url = site['site']
+        self.order.append('')
+        self.disallow.append(site['disallow'])
+
+    def check_allow(self, path):
+        return (path in self.disallow)
+
+    def bfs_visit(self):
+        visit_quantity = 0
+        while visit_quantity < self.MAX_VISITS:
+            html = requests.get(self.url + self.order.pop(0))
+            soup = BeautifulSoup(html.text, 'html.parser')
+            links = soup.find_all('a', href=True)
+            for l in links:
+                print (l['href'])
+                print ('\n\n')
+            time.sleep(1)
+            visit_quantity += 1
+
 if (__name__ == "__main__"):
     p = PreProcessing("../../site.txt")
-    p.print_info()
+    sites = p.get_sites_info()
+    for s in sites:
+        c = CrawlerBFS(s)
+        c.bfs_visit()
+        break
