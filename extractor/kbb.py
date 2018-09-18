32f7d8033b8aeb7d9b1b4190a6b60712a0061a5a
jailson <jailson@motorola.com>
2018-09-18 11:56:43 -0300

    table ok

--- extractor/kbb.py
+++ extractor/kbb.py
@@ -6,8 +6,24 @@ print(page.status_code)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-tabela_text = soup.find_all(class_="details-list")[0].get_text()
-tabela = tabela_text.split("\n")
+tabela_class = soup.find_all(class_="details-list")[0].get_text()
+tabela = tabela_class.split("\n")
+
+tabela = filter(None, tabela)
 
 for item in tabela:
-    print(item)
+    if(item.split()[0] == "Fuel"):
+        fuel = item.split(":")[-1].strip().replace('\n','')
+    if(item.split()[0] == "Exterior"):
+        exterior_color = item.split(":")[-1].strip().replace('\n','')
+    if(item.split()[0] == "Interior"):
+        interior_color = item.split(":")[-1].strip().replace('\n','')
+    if(item.split()[0] == "Engine:"):
+        engine = item.split(":")[-1].strip().replace('\n','')
+
+print(fuel, interior_color)
+print(exterior_color)
+print(engine)
+'''
+vin_class = soup.find_all(class_= "paragraph-one vin")[0].get_text()
+vin = vin_class.'''
