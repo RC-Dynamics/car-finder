44585a718f35808f381c0057c943048826c97efb
Roberto Fernandes <betocostaf@gmail.com>
2018-09-18 22:04:03 -0300

    Fixing pre_processing testing code

--- crawler/shared/pre_processing.py
+++ crawler/shared/pre_processing.py
@@ -59,5 +59,5 @@ class PreProcessing:
         return self.sites
 
 if (__name__ == "__main__"):
-    p = PreProcessing("../site.txt")
+    p = PreProcessing("../../site.txt", True)
     p.print_info()
