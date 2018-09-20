c6f8ab5700a737e91c7ddaf7ccadb5a0df8d2edd
jailson <jailson@motorola.com>
2018-09-20 11:24:47 -0300

    kijiji OK. Ainda precisa de reestruturação

--- extractor/kijiji.py
+++ extractor/kijiji.py
@@ -6,18 +6,33 @@ print(page.status_code)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+price = soup.find(class_="currentPrice-2872355490").get_text()
+
+title = soup.title.get_text().split("|")[0]
+#print(title)
+
+description = soup.find(class_="descriptionContainer-2901313666").get_text().split("Description")[-1].strip().replace('\n','')
+#print(description)
+
 tabela = soup.find_all(class_="itemAttribute-2841032265")
-
-'''str1 = "vamos la pracasa cara"
-str2 = str1.find("pra")
-print(str2)'''
-
-
 
 for item in tabela:
     if(item.get_text().count("Fuel") > 0 ):
         fuel = item.get_text().split("Type")[-1].strip().replace('\n','')
-    elif(item.get_text().count("Fuel") > 0 ):
-        fuel = item.get_text().split("Type")[-1].strip().replace('\n','')
+    elif(item.get_text().count("Transmission") > 0 ):
+        transmission = item.get_text().split("Transmission")[-1].strip().replace('\n','')
+    elif(item.get_text().count("Colour") > 0 ):
+        exterior_color = item.get_text().split("Colour")[-1].strip().replace('\n','')
 
-print(fuel)
+#print(fuel)
+#print(transmission)
+
+'''
+price
+fuel
+exterior_color
+transmission
+url
+title
+description
+'''
