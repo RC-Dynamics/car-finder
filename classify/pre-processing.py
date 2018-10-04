63dd7b29754bb2304b0cbffa590bcc5c95e61f40
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-10-04 17:16:57 -0300

    saving raw corpus in files

--- classify/pre-processing.py
+++ classify/pre-processing.py
@@ -10,7 +10,9 @@ from sklearn.feature_selection import mutual_info_classif
 import os as sys
 import csv
 from tqdm import tqdm
+
 import numpy as np
+import pickle
 
 def removeSpecial(txt):
     symbols = ['\n','/', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '|', '=', ':', '\\', '*', '>', '<']
@@ -20,7 +22,7 @@ def removeSpecial(txt):
 
 
 def makeBoW(name, texts, words):
-    with open('./data/'+name+".csv", 'w') as outcsv:
+    with open('./data/dataset/'+name+".csv", 'w') as outcsv:
         writer = csv.writer(outcsv)
         words_list = ["name"] + words + ['status']
         writer.writerow(words_list)
@@ -45,13 +47,13 @@ def makeBoW(name, texts, words):
 
 
 
-def main():
+def raw_corpus():
     corpus = set()
-    corpus_txt = []
-    file_txt = []
     files = {}
+    corpus_txt = []
+    Y  = []
 
-################# Collecting all corpus ###################
+################# Creating all corpus ###################
     for file in tqdm(sys.listdir("./data/txt")):
         if(file.endswith(".txt")):
             f = open("./data/txt/"+file, "r+")
@@ -59,84 +61,113 @@ def main():
             f.close()
             page = removeSpecial(page)
             corpus =  corpus | set(word_tokenize(page))
-            corpus_txt.append(page)
-            file_txt.append(file)
             files[file] = list(set(word_tokenize(page)))
-    
-    # Saving the corpus 
-    # f = open('./data/raw-data.txt', 'w')
-    # f.write(str(list(corpus))[1:-1])
-    # f.close()
 
-#     # Creating and saving the dataset raw as csv
-#     makeBoW("db1", files, list(corpus))
+            corpus_txt.append(page)
+            if file.endswith("pos.txt"):
+                Y.append(1)
+            else:
+                Y.append(0)
+
+    with open ("./data/dataset/corpus.cp", 'wb') as fp:
+        pickle.dump(list(corpus), fp)
+    
+    with open ("./data/dataset/files.cp", 'wb') as fp:
+        pickle.dump(files, fp)
+    
+    with open ("./data/dataset/corpus_txt.cp", 'wb') as fp:
+        pickle.dump(corpus_txt, fp)
+
+    with open ("./data/dataset/Y.cp", 'wb') as fp:
+        pickle.dump(Y, fp)
+    
+    return corpus, files, corpus_txt, Y
+
+def getRawCorpus():
+    with open ("./data/dataset/corpus.cp", 'wb') as fp:
+        pickle.dump(list(corpus), fp)
+    
+    with open ("./data/dataset/files.cp", 'wb') as fp:
+        pickle.dump(files, fp)
+    
+    with open ("./data/dataset/corpus_txt.cp", 'wb') as fp:
+        pickle.dump(corpus_txt, fp)
+
+    with open ("./data/dataset/Y.cp", 'wb') as fp:
+        pickle.dump(Y, fp)
+
+def bow_raw(files, corpus):
+    # Creating and saving the dataset raw as csv
+    print("Raw: ")
+    makeBoW("db1", files, list(corpus))
                 
+def bow_lower(files):
+################### FILTERS - LOWER #######################
+    corpus_lower = set ()
+    for key in files: #LOWER
+        for w in range(len(files[key])):
+            files[key][w] = (files[key][w]).lower()
+        corpus_lower |= set(files[key])
+        files[key] = list(set(files[key]))
+    corpus_lower = list(corpus_lower)
 
-# ################### FILTERS - LOWER #######################
-#     corpus_lower = set ()
-#     for key in files: #LOWER
-#         for w in range(len(files[key])):
-#             files[key][w] = (files[key][w]).lower()
-#         corpus_lower |= set(files[key])
-#         files[key] = list(set(files[key]))
-#     corpus_lower = list(corpus_lower)
+    print("Lower: ")
+    makeBoW("db2", files, corpus_lower)
+    return files, corpus_lower
 
-#     print("Lower: ", end = "")
-#     makeBoW("db2", files, corpus_lower)
+def bow_stopwords(files):
+#################### FILTERS - STOPWORDS ####################
+    stopWords = set(stopwords.words('english'))
+    corpus_stop = set()
+    for key in files:
+        files[key] = list(set(files[key]) - stopWords)
+        corpus_stop |= set(files[key])
+    corpus_stop = list(corpus_stop)
+
+    print("Lower + noStopWords: ")
+    makeBoW("db3", files, corpus_stop)
+    return files, corpus_stop
+
+def bow_stemming(files):
+################### FILTERS - STEMMING #####################
+    ps = PorterStemmer()
+    corpus_stem = set()
+    for key in files:
+        for w in range(len(files[key])):
+            files[key][w] = ps.stem(files[key][w])
+        corpus_stem |= set(files[key])
+        files[key] = list(set(files[key]))
     
-# #################### FILTERS - STOPWORDS ####################
+    corpus_stem = list(corpus_stem)
 
-#     stopWords = set(stopwords.words('english'))
-#     for key in files:
-#         files[key] = list(set(files[key]) - stopWords)
-#     corpus_stop = list(set(corpus_lower) - stopWords)
+    print("Lower + noStopWords + Stemming: ")
+    makeBoW("db4", files, corpus_stem)
+    return files, corpus_stem
 
-#     print("Lower + noStopWords: ", end = "")
-#     makeBoW("db3", files, corpus_stop)
-    
+def bow_vectorizer1(files, corpus_txt):
+################### FILTERS  - sklearn - max df = 0.9 ####################
 
+    vectorizer = TfidfVectorizer(max_df = 0.9, max_features = 1000)
+    vectorizer.fit_transform(corpus_txt)
 
-# ################### FILTERS - STEMMING #####################
+    print("Lower + noStopWords + max df x idf + : ")
+    makeBoW("db5", files, vectorizer.get_feature_names())
+    return vectorizer.get_feature_names()
 
-#     ps = PorterStemmer()
-#     corpus_stem = set()
-#     for key in files:
-#         for w in range(len(files[key])):
-#             files[key][w] = ps.stem(files[key][w])
-#         corpus_stem |= set(files[key])
-#         files[key] = list(set(files[key]))
-    
-#     corpus_stem = list(corpus_stem)
+def bow_vectorizer2(files, corpus_txt):
+################### FILTERS - max df = 0.8 # min df = 0.2 #############
 
-#     print("Lower + noStopWords + Stemming: ", end = "")
-#     makeBoW("db4", files, corpus_stem)
+    vectorizer = TfidfVectorizer(max_df = 0.8, min_df = 0.2, max_features = 1000)
+    vectorizer.fit_transform(corpus_txt)
 
-# ################### FILTERS  - sklearn - max df = 0.9 ####################
+    print("Lower + noStopWords + max & min df x idf : ")
+    makeBoW("db6", files, vectorizer.get_feature_names())
+    return vectorizer.get_feature_names()
 
-#     vectorizer = TfidfVectorizer(max_df = 0.9, max_features = 1000)
-#     vectorizer.fit_transform(corpus_txt)
-
-#     print("Lower + noStopWords + max df x idf + : ", end = "")
-#     makeBoW("db5", files, vectorizer.get_feature_names())
-
-# ################### FILTERS - max df = 0.8 # min df = 0.2 #############
-
-#     vectorizer = TfidfVectorizer(max_df = 0.8, min_df = 0.2, max_features = 1000)
-#     vectorizer.fit_transform(corpus_txt)
-
-#     print("Lower + noStopWords + max & min df x idf + : ", end = "")
-#     makeBoW("db6", files, vectorizer.get_feature_names())
-
-################### FILTERS - Info Gain ################################
-
+def bow_infogain(files, corpus_txt, Y):
+################## FILTERS - Info Gain ################################
     cv = CountVectorizer(max_df=0.9, min_df=0.05, max_features=1000, stop_words='english')
     vector = cv.fit_transform(corpus_txt)
-    Y = []
-    for f in file_txt:
-        if f.endswith("pos.txt"):
-            Y.append(1)
-        else:
-            Y.append(0)
     
     res = dict(zip(cv.get_feature_names(), mutual_info_classif(vector, Y, discrete_features=True)))
     avg = np.average(list(res.values()))
@@ -146,9 +177,35 @@ def main():
     for key, value in res.items():
         if(value > avg):
             corpus_info_gain.append(key)
-    print("Lower + noStopWords + InfoGain: ", end = "")
+    print("Lower + noStopWords + max & min df x idf + InfoGain: ")
     makeBoW("db7", files, corpus_info_gain)
+    
+    return corpus_info_gain
+
             
+def main():
+
+    # corpus, files, corpus_txt, Y = raw_corpus()
+
+
+    
+    bow_raw(files, corpus)
+
+    files_low, corpus_low = bow_lower(files)
+
+    files_stop, corpus_stop = bow_stopwords(files_low)
+
+    files_stemming, corpus_stemming = bow_stemming(files_stop)
+
+######
+
+    corpus_vec1 = bow_vectorizer1(files, corpus_txt)
+
+    corpus_vec2 = bow_vectorizer2(files, corpus_txt)
+
+    corpus_infogain = bow_infogain(files, corpus_txt, Y)
+
+    
 
 
 if __name__ == "__main__":
