4108b3e456ec2464dd2bf2ec58914e4ceb6cb0a3
Roberto <rcf6@cin.ufpe.br>
2018-10-05 17:59:09 -0300

    Creating files to start training method

--- crawler/ml/train.py
+++ crawler/ml/train.py
@@ -1,4 +1,65 @@
 import pandas as pd
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
+import os
+from nltk.tokenize import word_tokenize
 import pickle
+import pprint
+
+class URL_Classify:
+    path = ''
+    debug = False
+
+    def __init__(self, links_path, debug=False):
+        self.path = links_path
+        self.debug = debug
+        if self.debug:
+            self.pp = pprint.PrettyPrinter(indent=4)
+
+    def remove_special(self, txt):
+        symbols = ['\n','/', '.com', '.uk', '&', '+', '%', 'www.', '_', '.ca', '.co', 'jhtml', 'html', 'https', 'http', ',', '.', '"', '-', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '|', '=', ':', '\\', '*', '>', '<']
+        for a in symbols:
+            txt = txt.replace(a, " ")
+        return txt
+
+    def make_raw_corpus(self):
+        corpus = set()
+        files = {}
+        corpus_txt = []
+        Y  = []
+        link = 0
+        for file in os.listdir('./' + self.path):
+            if(file.endswith(".csv")):
+                data = pd.read_csv(self.path + '/' + file)
+                for _, row in data.iterrows():
+                    words = self.remove_special(row['link'])
+                    corpus =  corpus | set(word_tokenize(words))
+                    files[link] = list(set(word_tokenize(words)))
+
+                    Y.append(int(row['status']))
+                    link += 1
+        if self.debug:   
+            self.pp.pprint (files)
+            self.pp.pprint (Y)
+            self.pp.pprint (corpus)
+        return corpus, files, Y
+
+    def make_bow(self, texts, words, Y):
+        words_list = ["name"] + words + ['status']
+        for idx, obj in texts.items():
+            row = [idx]
+            for w in words:
+                if w in obj:
+                    row.append(1)
+                else:
+                    row.append(0)
+            row.append(Y[idx])
+        if self.debug:
+            self.pp.pprint(words_list)
+        return words_list
+
+    def train (self):
+        corpus, files, Y = self.make_raw_corpus()
+        words_list = self.make_bow(files, list(corpus), Y)
+
+if (__name__ == "__main__"):
+    classify = URL_Classify('data/links', debug=True)
+    classify.train()
