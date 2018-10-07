2b30e441734cb15f3b04d659201e1c37e3f25879
Roberto Fernandes <betocostaf@gmail.com>
2018-10-07 17:57:36 -0300

    Adding code to load model

--- crawler/ml/model.py
+++ crawler/ml/model.py
@@ -1,4 +1,26 @@
-import pandas as pd
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
+import nltk
+nltk.download('punkt')
+
 import pickle
+import pprint
+from nltk.tokenize import word_tokenize
+from sklearn import svm
+
+def remove_special(txt):
+    symbols = ['\n','/', '.com', '.uk', '&', '+', '%', 'www.', '_', '.ca', '.co', 'jhtml', 'html', 'https', 'http', ',', '.', '"', '-', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '|', '=', ':', '\\', '*', '>', '<']
+    for a in symbols:
+        txt = txt.replace(a, " ")
+    return txt
+
+def svr_link_ranking(url):
+    svr_mode = pickle.load(open('svr_model.sav', 'rb'))
+    corpus = pickle.load(open('corpus.sav', 'rb'))
+    url = remove_special(url)
+    url = list(set(word_tokenize(url)))
+    row = []
+    for word in corpus:
+        if word in url:
+            row.append(1)
+        else:
+            row.append(0)
+    return svr_mode.predict([row])[0]
