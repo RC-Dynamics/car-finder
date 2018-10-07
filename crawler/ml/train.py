38298a8444ccba2b22ac4984998f3fc25f8d6334
Roberto Fernandes <betocostaf@gmail.com>
2018-10-07 16:50:36 -0300

    Saving SVR first model

--- crawler/ml/train.py
+++ crawler/ml/train.py
@@ -1,8 +1,13 @@
+import nltk
+nltk.download('punkt')
+
 import pandas as pd
 import os
 from nltk.tokenize import word_tokenize
 import pickle
 import pprint
+from sklearn import svm
+
 
 class URL_Classify:
     path = ''
@@ -36,32 +41,44 @@ class URL_Classify:
 
                     Y.append(int(row['status']))
                     link += 1
-        # if self.debug:
-        #     self.pp.pprint (files)
-        #     self.pp.pprint (Y)
-        #     self.pp.pprint (corpus)
+        if self.debug:
+            self.pp.pprint (files)
+            self.pp.pprint (Y)
+            self.pp.pprint (corpus)
+        if self.debug:
+            print(len(corpus))
+            print(len(files))
+            print(len(Y))
         return corpus, files, Y
 
     def make_bow(self, texts, words, Y):
         bow = []
-        bow.append(['name'] + words + ['Y'])
+        # bow.append(['name'] + words + ['Y'])
         for idx, obj in texts.items():
-            row = [idx]
+            row = []
+            # row = [idx]
             for w in words:
                 if w in obj:
                     row.append(1)
                 else:
                     row.append(0)
-            row.append(Y[idx])
+            # row.append(Y[idx])
             bow.append(row)
         if self.debug:
-            print(bow)
+            print(len(bow))
         return bow
 
     def train (self):
         corpus, files, Y = self.make_raw_corpus()
-        bow = self.make_bow(files, list(corpus), Y)
+        X = self.make_bow(files, list(corpus), Y)
+
+        svr_model = svm.SVR(gamma='auto')
+        svr_model.fit(X, Y)
+        # print (svr_model.predict([X[0]]))
+
+        pickle.dump(svr_model, open('svr_model.sav', 'wb'))
+        pickle.dump(list(corpus), open('corpus.sav', 'wb'))
 
 if (__name__ == "__main__"):
-    classify = URL_Classify('data/links', debug=True)
+    classify = URL_Classify('data/links', debug=False)
     classify.train()
