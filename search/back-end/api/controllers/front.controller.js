7b07d84d3cc6be32bc0bd0b5d8ec3d58faa80bb0
Roberto Fernandes <betocostaf@gmail.com>
2018-11-18 10:22:52 -0300

    Adding first visualizartion on front-end

--- search/back-end/api/controllers/front.controller.js
+++ search/back-end/api/controllers/front.controller.js
@@ -1,10 +1,22 @@
 
 var search_car = (req, res) => {
     console.log(req.query);
-    res.status(200).json({
-        // TODO: Set response to send to front
-        a : "abc"
-    });
+    // TODO: Set response to send to front
+    res.status(200).json([{
+        title : "BMW SERIES 3 2014 AUTOMATIC RED",
+        transmission: "Manual",
+        colour: "Red",
+        price: "U$ 20.000",
+        milage: "40000 miles",
+        link: "https://www.kijiji.ca/"
+    }, {
+        title : "TOYOTA COROLLA",
+        transmission: "Automatic",
+        colour: "Black",
+        price: "U$ 4.500",
+        milage: "80000 miles",
+        link: "https://shift.com/"
+    }]);
 }
 
 
--- search/front-end/src/first-page.js
+++ search/front-end/src/first-page.js
@@ -8,8 +8,18 @@ $(document).ready(function () {
             data: form.serialize(),
             type: "GET",
             success: function (responseData) {
-                // TODO: Display data from server
-                console.log(responseData);
+                // console.log(responseData);
+                var result_div = $("#show-res-data");
+                responseData.forEach(element => {
+                    
+                    result_div.append(`<div>
+                    <a href="${element.link}"><h3>${element.title}</h3></a>
+                    <p class="item-info lead">Price: ${element.price}</p>
+                    <p class="item-info lead">Colour: ${element.colour}</p>
+                    <p class="item-info lead">Milage: ${element.milage}</p>
+                    <p class="item-info lead">Transmission: ${element.transmission}</p>
+                    <\div>`);
+                });
             },
             error: console.error
         });
