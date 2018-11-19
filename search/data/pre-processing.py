d38833b1914e90f7808339a0ce2b8ebbac9399e3
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-11-19 14:49:00 -0300

    adding invert index

--- search/data/pre-processing.py
+++ search/data/pre-processing.py
@@ -1,15 +1,17 @@
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 import os as sys
 import csv
+import json
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import pickle
 
 def removeSpecial(txt):
-    symbols = ['\n','/', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '|', '=', ':', '\\', '*', '>', '<']
+    symbols = ['\n','/', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '|', '=', ':', '\\', '*', '>', '<', '2c', '22', '3a', 'div', 'u002f', 'u002f', 'u003c', 'u003e']
     for a in symbols:
         txt = txt.replace(a, " ")
     return txt
@@ -60,30 +62,69 @@ def getRawCorpus():
     with open ("./corpus/corpus.cp", 'rb') as fp:
         corpus = pickle.load(fp)
     
-    with open ("./corpus/files.cp", 'rb') as fp:
+    with open ("./corpus/files_corpus.cp", 'rb') as fp:
         files = pickle.load(fp)
     
-    with open ("./corpus/corpus_txt.cp", 'rb') as fp:
+    with open ("./corpus/corpus_raw.cp", 'rb') as fp:
         corpus_txt = pickle.load(fp)
     
     return corpus, files, corpus_txt
                 
 def bowInfogain(files, corpus_txt):
 ################## FILTERS - Info Gain ################################
-    cv = CountVectorizer(max_df=0.9, min_df=0.05, max_features=1000, stop_words='english')
-    vector = cv.fit_transform(corpus_txt)
+    cv = CountVectorizer(max_df=0.9, min_df=0.05, stop_words='english')
+    
+    # print(cv.inverse_transform(vector))
+    # features = cv.get_feature_names()
+    # print (corpus_txt[0])
     # print (vector)
-
-    return []
+    # print (features)
+    return cv.fit_transform(corpus_txt), cv.get_feature_names()
 
             
 def main():
-
-    corpus, files, corpus_txt = makeRawCorpus()
+    querry = ["chevrolet", "car"]
+    # corpus, files, corpus_txt = makeRawCorpus()
     
-    # corpus, files, corpus_txt, Y  = getRawCorpus()
+    corpus, files, corpus_txt = getRawCorpus()
 
-    corpus_infogain = bowInfogain(files, corpus_txt)
+    vector, features = bowInfogain(files, corpus_txt)
+    
+    keys = list(files.keys())
+    j = 0
+    dictionary = {}
+    for fet in features:
+        dictionary[fet] = []
+        # print(fet)
+        i = 0
+        for vec in vector.toarray():
+            if vec[j] != 0:
+                temp = {}
+                temp["doc"] = int(i)
+                temp["freq"] = int(vec[j])
+                dictionary[fet].append(temp)
+            i += 1
+        j += 1
+    with open('index.json', 'w') as fp:
+        json.dump(dictionary, fp)
+    
+
+    
+
+
+
+    # cosine_similarities = linear_kernel(vector[0:1], vector).flatten()
+    # print (cosine_similarities)
+    # print (vector.shape)
+    # document = 0
+    # for i in vector:
+    #     print(i)
+    #     document += 1
+    #     break
+        
+
+
+
     
 
 
